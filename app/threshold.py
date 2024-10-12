1fba17a9301e2cda140348be97e5c30f70e13774
blogerlu <blogerlu>
2024-10-13 00:46:46 +0300

    fix

--- app/threshold.py
+++ app/threshold.py
@@ -20,13 +20,14 @@ class Threshold:
 
     @staticmethod
     def read_image(path_to_image: str) -> np.ndarray:
+        print(path_to_image)
         image = cv2.imread(path_to_image)
         return image
 
     def get_path_to_image(self, row: pd.DataFrame, path_to_folder: str) -> np.ndarray:
         tp = self.data_types[np.argmax([row[dt.replace("val", "valid")] for dt in self.data_types])]
 
-        path_to_image = os.path.join(path_to_folder, tp, f"{row['attachment_id']}.npy")
+        path_to_image = os.path.join(path_to_folder, tp, f"{row['attachment_id']}.jpg")
 
         return path_to_image
 
@@ -36,8 +37,16 @@ class Threshold:
         results = []
 
         for user_id, group in grouped:
-            vectors = np.array([self.get_embedding(self.read_image(self.get_path_to_image(row, path_to_folder))) for _, row in group])
-            print(vectors)
+
+            if len(group) < 2:
+#                results.append({'user_id': user_id, 'average_cosine_distance': -100})
+                continue
+            try:
+               vectors = np.array([self.get_embedding(self.read_image(self.get_path_to_image(row, path_to_folder))) for _, row in group.iterrows()])
+            except IndexError:
+               print('error')
+               continue
+#            print(vectors)
             if len(vectors) < 2:
                 avg_distance = -100
             else:
@@ -46,9 +55,11 @@ class Threshold:
                 upper_triangle_indices = np.triu_indices(len(vectors), k=1)
                 pairwise_distances = cosine_dist[upper_triangle_indices]
 
-                avg_distance = np.mean(pairwise_distances)
+                avg_distance = 1 - np.mean(pairwise_distances)
 
             results.append({'user_id': user_id, 'average_cosine_distance': avg_distance})
+            dt = pd.DataFrame(results)
+            dt.to_csv('/home/blogerlu/biometrics/biometrics-hack/meta_res.csv', index=False)
 
         return pd.DataFrame(results)
 
@@ -57,7 +68,7 @@ if __name__ == "__main__":
 
     app = FaceAnalysis(providers=["CUDAExecutionProvider", "CPUExecutionProvider"])
     app.prepare(ctx_id=0, det_size=(640, 640))
-    data = pd.read_csv('../meta.csv')
+    data = pd.read_csv('/mnt/sda1/hackathons/biometrics-hack/archive/annotations/meta/meta.csv')
     trch = Threshold(app, data)
-    dt = trch.process_user_data()
+    dt = trch.process_user_data('/mnt/sda1/hackathons/biometrics-hack/archive/images')
     dt.to_csv('../meta_res.csv', index=False)
